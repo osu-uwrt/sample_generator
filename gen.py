b12e6615737775fff149d5a91715a94d6f0d472a
OSU UWRT <osu.uwrt@gmail.com>
2021-01-31 13:39:54 -0500

    Adding gitignore and changing code

--- gen.py
+++ gen.py
@@ -11,7 +11,7 @@ opt = lambda: None
 opt.spp = 50 
 opt.width = 512
 opt.height = 512 
-opt.nb_frames = 10
+opt.nb_frames = 40
 opt.out = '01_simple_scene.png'
 
 # headless - no window
@@ -53,37 +53,6 @@ floor = visii.entity.create(
     material = visii.material.create("material_floor")
 )
 
-# Lets make our floor act as a mirror
-mat = floor.get_material()
-# mat = visii.material.get("material_floor") # <- this also works
-
-# Mirrors are smooth and "metallic".
-mat.set_base_color((0.19,0.16,0.19)) 
-mat.set_metallic(1) 
-mat.set_roughness(0)
-
-# Make the floor large by scaling it
-trans = floor.get_transform()
-trans.set_scale((5,5,1))
-
-# # Let's also add a sphere
-# sphere = visii.entity.create(
-#     name="sphere",
-#     mesh = visii.mesh.create_sphere("sphere"),
-#     transform = visii.transform.create("sphere"),
-#     material = visii.material.create("sphere")
-# )
-# sphere.get_transform().set_position((
-#         uniform(-5,5),
-#         uniform(-5,5),
-#         uniform(-1,3)
-#     ))
-# sphere.get_transform().set_scale((0.4, 0.4, 0.4))
-# sphere.get_material().set_base_color((0.1,0.9,0.08))  
-# sphere.get_material().set_roughness(0.7)   
-
-# # # # # # # # # # # # # # # # # # # # # # # # #
-
 # This function loads a signle obj mesh. It ignores 
 # the associated .mtl file
 mesh = visii.mesh.create_from_file("obj", "./garlic.obj")
@@ -106,10 +75,18 @@ obj_entity.get_material().set_roughness(0.7)
 obj_entity.get_material().set_specular(1)   
 obj_entity.get_material().set_sheen(1)
 
-
-# # # # # # # # # # # # # # # # # # # # # # # # #
-
 for i in range(opt.nb_frames):
+    obj_entity.get_transform().set_position((
+        uniform(-5,5),
+        uniform(0,-60),
+        uniform(-1,3)
+    ))
+    obj_entity.get_transform().set_rotation((
+        uniform(0,1), # X 
+        uniform(0,1), # Y
+        uniform(0,1), # Z
+        uniform(0,1)  # W
+    ))
     # Now that we have a simple scene, let's render it 
     visii.render_to_file(
         width = opt.width, 
@@ -117,10 +94,5 @@ for i in range(opt.nb_frames):
         samples_per_pixel = opt.spp,   
         file_path = 'data/garlic' + str(i) + '.png'
     )
-    obj_entity.get_transform().set_position((
-        uniform(-5,5),
-        uniform(-5,5),
-        uniform(-1,3)
-    ))
 
 visii.deinitialize()
